418e26d7781975b5d7822ace92f5aad3d400cc02
John Doe <rizharafif11@gmail.com>
2025-02-03 14:21:25 +0700

    [ADD] feat login

--- src/app/auth/signup/sign-up-form.tsx
+++ src/app/auth/signup/sign-up-form.tsx
@@ -8,6 +8,10 @@ import { cn } from "@/lib/utils";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useRegisterMutation } from "@/services/auth";
+import { signIn } from "next-auth/react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
 
 type UserAuthForm = {
   email: string;
@@ -35,6 +39,9 @@ function SignUpForm() {
   const [showConfirmationPassword, setShowConfirmationPassword] =
     useState(false);
 
+  const { toast } = useToast();
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const {
     handleSubmit,
     register,
@@ -42,9 +49,32 @@ function SignUpForm() {
   } = useForm<UserAuthForm>({
     resolver: yupResolver(schema),
   });
+  const [registerMutation] = useRegisterMutation()
 
-  const onSubmit = (data: UserAuthForm) => {
-    console.log("🚀 ~ onSubmit ~ data:", data);
+  const onSubmit = async (data: UserAuthForm) => {
+    try {
+      const res = await registerMutation(data).unwrap();
+
+      if (res.success) {
+        const user = await signIn('credentials', {
+          email: data.email,
+          password: data.password,
+          callbackUrl: searchParams.get("callbackUrl") || "/",
+          redirect: false
+        })
+
+        router.push(user?.url || "/")
+      } else {
+        toast({
+          title: "Something went wrong",
+          description: 'Please check your email and password',
+          variant: "destructive",
+          duration: 2000
+        })
+      }
+    } catch (error) {
+      console.log("🚀 ~ onSubmit ~ error:", error)
+    }
   };
 
   return (
---
+++ src/services/auth.ts
@@ -0,0 +1,32 @@
+import BaseResponse from "@/types/response";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+
+interface AuthResponse extends BaseResponse {
+    data: UserAuthForm
+}
+
+type UserAuthForm = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api",
+  }),
+  tagTypes: ["auth"],
+  endpoints: (builder) => ({
+    // mutation (POST, DELETE, PUT), query (SELECT)
+    register: builder.mutation<AuthResponse, UserAuthForm>({
+      query: (body) => ({
+        url: "/auth/sign-up",
+        method: "POST",
+        body,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation } = authApi;
